f0dbe66d30e223ef794efbbe39aaaa57c2b735ca
Tymeckia Kendall <tykend@gmail.com>
2019-03-06 01:13:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,7 @@ end
 def summon_captain_planet (veggies) # code an here
   # Your code here
   veggies.collect{|element| element.capitalize!+"!"}
+end
 
 def long_planeteer_calls# code an argument here
   # Your code here
