bd2aac4d1f1823e8cac8f211c0d3b0ce2c5214b8
Tymeckia Kendall <tykend@gmail.com>
2019-03-06 01:21:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls (fruits) # code an argument here
   fruits.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(contains_cheddar)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
    cheese_types.each do |cheese|
